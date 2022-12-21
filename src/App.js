9310cba0941edee809a14cbb78f91ea838b2f66e
tanukeshari <kesharitanu@gmail.com>
2022-12-21 17:31:16 +0530

    tanukeshari-expenseitem

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React with tanu
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import ExpenseItem from "./ExpenseItem";
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Lets get started!</h2>
+     
+      <ExpenseItem></ExpenseItem>
+
     </div>
   );
 }
-
 export default App;
---
+++ src/ExpenseItem.js
@@ -0,0 +1,26 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <div>18/12/2022</div>
+            <div>
+                <h2>food</h2>
+                <div>Rs 10</div>
+            </div>
+
+            <div>19/12/2022</div>
+            <div>
+                <h2>Petrol</h2>
+                <div>Rs 100</div>
+            </div>
+
+            <div>20/12/2022</div>
+            <div>
+                <h2>Movies</h2>
+                <div>Rs 200</div>
+            </div>
+        </div>
+
+
+    );
+}
+export default ExpenseItem;
