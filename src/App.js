cd0df3b2454b4bbd6212bcfcb0fdf4959b9486c3
tanukeshari <kesharitanu@gmail.com>
2022-12-22 16:35:46 +0530

    propsreact.js-

--- src/App.js
+++ src/App.js
@@ -26,11 +26,54 @@
 
 import ExpenseItem from "./ExpenseItem";
 function App(){
+
+  const expenses=[
+    {
+      id:'e1',
+      title:'Brush',
+      amount:20,
+      Date:new Date(2021, 6,17), 
+    },
+    { id: 'e2',
+    title:' food ',
+    amount:10,
+     Date : new Date(2022,2,20)},
+    { id: 'e3',
+    title:'petrol', 
+    amount :100,
+     Date: new Date(2022, 3, 21) },
+
+    { id: 'e4',
+    title:'movies', 
+    amount: 200, 
+    Date: new Date (2022, 4, 22)},
+    
+  ];
   return (
     <div>
       <h2>Lets get started!</h2>
      
-      <ExpenseItem></ExpenseItem>
+      
+      <ExpenseItem title={expenses[0].title}
+      amount={expenses[0].amount}
+      date={expenses[0].date}>
+      </ExpenseItem>
+
+      <ExpenseItem title={expenses[1].title}
+      amount={expenses[1].amount}
+      date={expenses[1].date}>
+      </ExpenseItem>
+
+
+      <ExpenseItem title={expenses[2].title}
+      amount={expenses[2].amount}
+      date={expenses[2].date}>
+      </ExpenseItem>
+      
+      <ExpenseItem title={expenses[3].title}
+      amount={expenses[3].amount}
+      date={expenses[3].date}>
+      </ExpenseItem>
 
     </div>
   );
