48d3178f526139922173d47ce2912d95262ee0a1
tanukeshari <kesharitanu@gmail.com>
2023-01-14 17:48:57 +0530

    change-expense

--- src/component/Expenses/Expenseitem.js
+++ src/component/Expenses/Expenseitem.js
@@ -1,23 +1,38 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './Expenseitem.css';
 
-const  Expenseitem=(props)=>{
-  const clickHandler= ()=>{
-console.log('clicked!!')
+
+const  Expenseitem =(props)=>{
+
+ const [amount, setTitle]= useState(props.amount);
+  
+ const clickHandler = ()=>{
+    console.log("Updated!")
+  setTitle("100");
+  console.log(amount);
   };
+
+  const deleteTitle=()=>{
+    console.log("Deleted!")
+  }
   
     return(
       <Card className="expense-item">
     
       <ExpenseDate date={props.date }/>      
-    <div className="expense-item__discription">
+    <div className="expense-item__discription"> 
         <h2>{props.title}</h2>
-        <div className="expense-item__price">{props.amount}</div>
+        <div className="expense-item__price">{amount}</div>
+   
+    <button onClick={clickHandler()}>changeTitle</button>
+    <button onClick={deleteTitle}></button>
     </div>
-    <button onClick={clickHandler()}>Edit</button>
+    <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
+        <button onClick={clickHandler}>changeExpense</button>
  </Card>
      );
-}
+    }
 export default Expenseitem;
