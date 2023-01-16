143f72a25f8edde71979a265ae286395b5e29ec7
tanukeshari <kesharitanu@gmail.com>
2023-01-16 14:58:10 +0530

    newoBJECt

--- src/component/NewExpense/ExpenseForm.js
+++ src/component/NewExpense/ExpenseForm.js
@@ -1,16 +1,22 @@
 import React from "react";
 
 import './ExpenseForm.css';
+
 const ExpenseForm=()=>{
-return <form>
+    
+const titleChangeHandler=(event)=>{
+    console.log(event.target.value);
+}
+return (
+<form>
     <div className="new-expense__controls">
         <div className="new-expense__control">
-            <label>Title</label>
-            <input type="text" />
+            <label>ExpenseTitle</label>
+            <input type="text" onChange={titleChangeHandler} />
 
         </div>
         <div className="new-expense__control">
-            <label>Amount</label>
+            <label>ExpenseAmount</label>
             <input type="number" min="10" step="1000" />
             
         </div>
@@ -24,5 +30,6 @@ return <form>
         <button type="submit">Add Expense</button>
     </div>
 </form>
+)
 }
 export default ExpenseForm;
