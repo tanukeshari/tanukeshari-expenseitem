01ffcbe067df2a851a2eeaed113af82af4e3be36
tanukeshari <kesharitanu@gmail.com>
2023-01-25 20:57:35 +0530

    logging-page

--- src/App.js
+++ src/App.js
@@ -1,50 +1,40 @@
-   import React, { useState }from 'react';
-   
-   import NewExpense from './component/NewExpense/NewExpense';
- import Expenses from './component/Expenses/Expenses';
+import React, { useEffect, useState } from 'react';
 
+import Login from './Components/Login/Login';
+import Home from './Components/Home/Home';
+import MainHeader from './Components/MainHeader/MainHeader';
 
-  const DUMMY_EXPENSES = [
-    {
-    id: 'e1',
-    title: 'oil',
-    amount: 120,
-    date: new Date(2020, 1, 1),
-  },
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  { id: 'e2', title: 'radio', amount: 12000, date: new Date(2022, 1, 12) },
-  {
-    id: 'e3',
-    title: 'mobile',
-    amount: 10000,
-    date: new Date(2022, 1, 13),
-
-  },
-  {
-    id: 'e4',
-    title: 'rice',
-    amount: 130,
-    date: new Date(2019, 1, 7)
-  },
-  ]
-
-  const App = () => {
-  const[expenses, setExpenses] = useState(DUMMY_EXPENSES);
-        
-    const addExpenseHandler = (expense) => { 
-         setExpenses((prevExpenses) => {
-          return [expense, ...prevExpenses];
-        });
-
-
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', '1')
+    setIsLoggedIn(true);
   };
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+    localStorage.removeItem('isLoggedIn');
+  };
+
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+     if ( storedUserLoggedInInformation === '1'){
+      setIsLoggedIn(true);
+     }
+  }, []);
+
   return (
-    <div>
-        <NewExpense onAddExpense={addExpenseHandler} />  
-      <Expenses items={expenses} />
-    </div>
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
   );
 }
 
 export default App;
-
--- src/card.js
+++
@@ -1,9 +0,0 @@
-// import React from "react";
-
-// const card=(props)=>{
-//     const classes='card' +props.className;
-
-//     return<div> className={classes}{props.children}</div>
-// }
-
-// export default card;
